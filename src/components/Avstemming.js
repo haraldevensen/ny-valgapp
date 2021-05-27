91d4a53b528a63cd04c000497dc58085642ed3e9
haraldevensen <sebastian.evensen@gmail.com>
2021-05-27 21:51:37 +0200

    avstemming

--- src/components/Avstemming.js
+++ src/components/Avstemming.js
@@ -1,12 +1,44 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import Navbar from "./Navbar";
+import { db } from "../firebase";
 
-export default function Avstemming() {
-  return (
-    <>
-      <Navbar />
-      <Container fluid>Her kommer det spenstig innhold</Container>
-    </>
-  );
+class Avstemming extends React.Component {
+  state = {
+    users: null 
+  }
+
+  componentDidMount(){
+    console.log('mounted')
+    db.collection('users')
+      .get()
+      .then(snapshot => {
+        const users = []
+        snapshot.forEach(doc => {
+          const data = doc.data()
+          users.push(data)
+        })
+        this.setState({ users: users })
+        // console.log(snapshot)
+      })
+      .catch(error => console.log(error))
+  }
+  
+  render(){
+    return(
+      <div className="Avstemming">
+        <h1>Liste over kandidater</h1>
+        {
+          this.state.users &&
+          this.state.users.map( user => {
+            return (
+              <div>
+                <p>{user.name}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
+
+export default Avstemming
